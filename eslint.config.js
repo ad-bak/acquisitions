20758639ef99b28802003d55382c237b2291308c
adbak <113508739+ad-bak@users.noreply.github.com>
2025-10-19 16:38:46 +0200

    prettier + eslint

---
+++ eslint.config.js
@@ -0,0 +1,61 @@
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier/recommended';
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        URL: 'readonly',
+        settimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setinterval: 'readonly',
+        clearinterval: 'readonly',
+      },
+    },
+    rules: {
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'object-shorthand': 'error',
+      'prefer-arrow-callback': 'error',
+    },
+  },
+  {
+    files: ['tests/**/*.js'],
+    languageOptions: {
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        jest: 'readonly',
+      },
+    },
+  },
+  {
+    ignores: [
+      'node_modules/**',
+      'dist/**',
+      'coverage/**',
+      'logs/**',
+      'drizzle/**',
+    ],
+  },
+];
